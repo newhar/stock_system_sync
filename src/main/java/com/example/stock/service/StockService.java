e03451fee0b8fe74e7a2b0bc5e6688bfc72020e0
newhar <gtqh2yj@gmail.com>
2022-09-14 16:14:16 +0900

    feat: pessimistic lock 재고 감소 로직

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -13,6 +13,9 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
+    /*
+        기본 재고 감소 로직
+     */
     @Transactional
     public void decreaseStock(Long productId, Long quantity) {
         Stock product = stockRepository.findByProductId(productId).orElseThrow();
@@ -22,8 +25,24 @@ public class StockService {
         stockRepository.saveAndFlush(product);
     }
 
+    /*
+        synchronized 키워드 를 사용하고 그 안에서 transaction 을 진행함으로 써 한 프로세스에서는 하나의 메소드가 순차적으로 실행될 수 있도록 한다.
+        하지만 여러 프로세스(서버) 에서 요청이 들어올 경우 데이터베이스에 동시 접근 가능성이 있기 때문에 위험하다.
+     */
     public synchronized void decreaseStockWithSynchronized(Long productId, Long quantity) {
         Stock product = stockRepository.findByProductId(productId).orElseThrow();
+
+        product.decreaseStock(quantity);
+
+        stockRepository.saveAndFlush(product);
+    }
+
+    /*
+        pessimisticLock 을 DB에 걸어서 처리한다. 실제 exclusive lock 을 거는 행위이다.
+     */
+    @Transactional
+    public void decreaseStockWithPerssimisticLock(Long productId, Long quantity) {
+        Stock product = stockRepository.findByProductIdWithPerssimistic(productId);
 
         product.decreaseStock(quantity);
 
--- src/test/java/com/example/stock/StockTest.java
+++ src/test/java/com/example/stock/StockTest.java
@@ -1,9 +1,7 @@
 package com.example.stock;
 
 import com.example.stock.domain.Stock;
-import com.example.stock.repository.StockPessimisticRepository;
 import com.example.stock.repository.StockRepository;
-import com.example.stock.service.StockPessimisticService;
 import com.example.stock.service.StockService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -24,22 +22,16 @@ public class StockTest {
     private StockRepository stockRepository;
     @Autowired
     private StockService stockService;
-//    @Autowired
-//    private StockPessimisticRepository stockPessimisticRepository;
-//    @Autowired
-//    private StockPessimisticService stockPessimisticService;
 
     @BeforeEach
     public void set() {
         Stock stock = new Stock(1L, 100L);
         stockRepository.saveAndFlush(stock);
-//        stockPessimisticRepository.saveAndFlush(stock);
     }
 
     @AfterEach
     public void setAfter() {
         stockRepository.deleteAll();
-//        stockPessimisticRepository.deleteAll();
     }
 
     @Test
@@ -50,32 +42,32 @@ public class StockTest {
         Assertions.assertThat(stock.getQuantity()).isEqualTo(99L);
     }
 
-//    @Test
-//    public void 동시_요청_재고감소_perssimistic() throws InterruptedException {
-//        int threadCount = 100;
-//        ExecutorService executorService = Executors.newFixedThreadPool(32);
-//        CountDownLatch latch = new CountDownLatch(threadCount);
-//        // CountDownLatch : 특정 쓰레드 숫자까지 다음 쓰레드를 진행하지 않도록 하기위함
-//        for (int i=0; i<threadCount; i++) {
-//            executorService.submit(() -> {
-//                try {
-//                    stockPessimisticService.decreaseStock(1L, 1L);
-//                } finally {
-//                    latch.countDown();
-//                }
-//            });
-//
-//        }
-//
-//        latch.await();
-//
-//        Stock stock = stockRepository.findByProductId(1L).orElseThrow();
-//
-//        Assertions.assertThat(stock.getQuantity()).isEqualTo(0L);
-//    }
+    @Test
+    public void 동시_요청_재고감소_perssimistic() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        // CountDownLatch : 특정 쓰레드 숫자까지 다음 쓰레드를 진행하지 않도록 하기위함
+        for (int i=0; i<threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decreaseStockWithPerssimisticLock(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findByProductId(1L).orElseThrow();
+
+        Assertions.assertThat(stock.getQuantity()).isEqualTo(0L);
+    }
 
     @Test
-    public void 실패케이스_동시_요청_재고감소() throws InterruptedException {
+    public void 동시_요청_재고감소() throws InterruptedException {
         int threadCount = 100;
         ExecutorService executorService = Executors.newFixedThreadPool(32);
         CountDownLatch latch = new CountDownLatch(threadCount);
@@ -95,11 +87,11 @@ public class StockTest {
 
         Stock stock = stockRepository.findByProductId(1L).orElseThrow();
 
-        Assertions.assertThat(stock.getQuantity()).isEqualTo(0L);
+        Assertions.assertThat(stock.getQuantity()).isNotEqualTo(0L);
     }
 
     @Test
-    public void 성공케이스_동시_요청_재고감소_withSynchronized() throws InterruptedException {
+    public void 동시_요청_재고감소_withSynchronized() throws InterruptedException {
         int threadCount = 100;
         ExecutorService executorService = Executors.newFixedThreadPool(32);
         CountDownLatch latch = new CountDownLatch(threadCount);
@@ -124,7 +116,7 @@ public class StockTest {
 
 
     @Test
-    public void 실패케이스_반복문_요청_재고감소() throws InterruptedException {
+    public void 반복문_요청_재고감소() throws InterruptedException {
         int threadCount = 100;
         for (int i=0; i<threadCount; i++) {
                     stockService.decreaseStock(1L, 1L);
